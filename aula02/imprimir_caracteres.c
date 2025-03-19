7a5e7c4015b0dbb261b0863eeb97b29afd719173
unknown <2412130069@ACADEMICO.IESB.BR>
2025-03-19 10:56:11 -0300

    adicionando arquivos da aula02

---
+++ aula02/imprimir_caracteres.c
@@ -0,0 +1,9 @@
+ #include <stdio.h>
+
+ int main(){
+    printf("opa\n");
+    printf("vamos \t aprender");
+    
+    return 0;
+
+ }
---
+++ aula02/pratica02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+        printf("\neduardo serra negra antunes\n");
+
+  return 0;
+}
